a9595fcb8d01b9f208a0e6a7f230008fdc067df9
Ben Lawton <benlawton@bens-mbp.home>
2024-07-26 18:54:24 +0100

    Fix SQL injection risk

--- tasks/fullStack/app/controllers/reviews_controller.rb
+++ tasks/fullStack/app/controllers/reviews_controller.rb
@@ -7,12 +7,12 @@ class ReviewsController < ApplicationController
   end
 
   def index
-    if params[:shop_id].present? && Shop.where("id = #{params[:shop_id]}").present?
+    if params[:shop_id].present? && Shop.where(id: params[:shop_id]).exists?
       params[:per_page] ||= 10
       offset = params[:page].to_i * params[:per_page]
 
       @data = []
-      products = Product.where("shop_id = #{params[:shop_id]}").sort_by(&:created_at)[offset..(offset + params[:per_page])]
+      products = Product.where(shop_id: params[:shop_id]).sort_by(&:created_at)[offset..(offset + params[:per_page])]
       products.each do |product|
         reviews = product.reviews.sort_by(&:created_at)[offset..(offset + params[:per_page])]
         @data << { product: product, reviews: reviews }
