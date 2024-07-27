bdcb04037fd7ec6dbd162bfd54d294969b6bc79f
Ben Lawton <benlawton@bens-mbp.home>
2024-07-27 13:31:54 +0100

    Extract tagging logic into a separate service

--- tasks/fullStack/app/controllers/reviews_controller.rb
+++ tasks/fullStack/app/controllers/reviews_controller.rb
@@ -23,11 +23,12 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    tags = TagService.tags_with_default(review_params[:product_id], review_params[:tags])
     ReviewCreationWorker.perform_async(review_params[:product_id],
                                        review_params[:body],
                                        review_params[:rating],
                                        review_params[:reviewer_name],
-                                       tags_with_default(review_params))
+                                       tags)
     flash[:notice] = 'Review is being created in background. It might take a moment to show up'
     redirect_to action: :index, shop_id: Product.find_by(id: review_params[:product_id]).shop_id
   end
@@ -36,22 +37,6 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:body, :rating, :reviewer_name, :tags, :product_id)
-  end
-
-  # Prepend `params[:tags]` with tags of the shop (if present) or DEFAULT_TAGS
-  # For simplicity, let's skip the frontend for `tags`, and just assume frontend can somehow magically send to backend `params[:tags]` as a comma-separated string
-  # The logic/requirement of tags is that:
-  #  - A review can have `tags` (for simplicity, tags are just an array of strings)
-  #  - If the shop has some `tags`, those tags of the shop should be part of the review's `tags`
-  #  - Else (if the shop doesn't have any `tags`), the default tags (in constant `DEFAULT_TAGS`) should be part of the review's `tags`
-  # One may wonder what an odd logic and lenthy comment, thus may suspect something hidden here, an easter egg perhaps.
-  def tags_with_default(params)
-    product = Product.find_by(id: params[:product_id])
-    default_tags = product.shop.tags || DEFAULT_TAGS
-    if params[:tags].present?
-      default_tags.concat(params[:tags].split(',')).uniq
-    end
-    default_tags
   end
 
   def set_shop
